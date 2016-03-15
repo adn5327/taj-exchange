d49b2d18fb76b42b9836ed0d07b63623a56b6897
Abhishek Deep Nigam <adn5327@gmail.com>
2016-03-15 18:29:14 -0500

    updating urls and adding rudimentary views

--- tajsite/exchange/views.py
+++ tajsite/exchange/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello. Welcome to the Taj Exchange")
 # Create your views here.
