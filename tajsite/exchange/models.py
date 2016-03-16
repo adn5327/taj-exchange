eece5338a32d01c13b713e75180efe18bd5bdd8e
TylerSplitt <tylersplitt@gmail.com>
2016-03-15 19:27:04 -0500

    Fixed syntax error and added relationships to models

--- tajsite/exchange/models.py
+++ tajsite/exchange/models.py
@@ -3,16 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Security(models.Model):
-	symbol = models.CharField(max_length=5)
+	symbol = models.CharField(primary_key=True,max_length=5)
 	volume = models.IntegerField(default=0)
 	inner_bid = models.IntegerField(default=0)
 	inner_ask = models.IntegerField(default=0)
 	fmv = models.IntegerField(default=0)
+	orders = models.ManyToManyField(Order)
+	accounts = models.ManyToManyField(Account)
 	#inner_bid, inner_ask, and fmv come from orders db
-	#NEED  'HAS' RELATIONSHIP WITH ACCOUNT
-	#NEED  'HAS' RELATIONSHIP WITH ORDER
-
-
 
 	def __str__(self):
 		return self.symbol
@@ -24,23 +22,24 @@ class Order(models.Model):
 	ask = models.BooleanField(default=False)
 	price = models.IntegerField(default=0)
 	amount = models.IntegerField(default=0)
-	order_id = models.IntegerField(default=0)
-	security = #relationship
-	#NEED  'HAS' RELATIONSHIP WITH SECURITY
+	order_id = models.IntegerField(primary_key=True,default=0)
+	security = models.ForeignKey(Security, on_delete=models.CASCADE)
+	account = models.ForeignKey(Account, on_delete=models.CASCADE)
 	#NEED  'TRADE' RELATIONSHIP WITH ORDER
-	#NEED  'PLACES' RELATIONSHIP WITH ACCOUNT
 	#need to update order_type, order_id
 
 	def __str__(self):
-		if(self.bid)
-			return str(self.order_id)+': BID on ' + self.security+' - +str(self.amount)+' at '+str(self.price) 
+		if(self.bid):
+			return str(self.order_id)+': BID on ' + self.security+' - +str(self.amount)+' at '+str(self.price)'
+		else:
+			return str(self.order_id)+': ASK on ' + self.security+' - +str(self.amount)+' at '+str(self.price)'
 class Account(models.Model):
 	name = models.CharField(max_length=20)
 	funds = models.IntegerField(default=0)
-	SSN = models.IntegerField(default=0)
-	account_num = models.IntegerField(default=0)
-	#NEED  'PLACES' RELATIONSHIP WITH ORDER
-	#NEED  'HAS' RELATIONSHIP WITH SECURITY
+	SSN = models.IntegerField(primary_key=True,unique=False,default=0)
+	account_num = models.IntegerField(primary_key=True,unique=False,default=0)
+	securities = models.ManyToManyField(Security)
+	orders = models.ManyToManyField(Order)
 
 	def __str__(self):
 		return self.name			
