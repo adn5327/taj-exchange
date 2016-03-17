ebc62cb5bd36f3a4dc9143c1cee4c3cfadefc79b
TylerSplitt <tylersplitt@gmail.com>
2016-03-17 02:47:00 -0500

    Refactor inner_bid/ask

--- tajsite/exchange/views.py
+++ tajsite/exchange/views.py
@@ -29,14 +29,8 @@ def order(request):
 			bid_orders = Order.objects.filter(order_security=o.order_security,bidask='BID').order_by('-price')
 			print ask_orders
 			print bid_orders
-			if o.bidask == 'ASK' and o.price < ask_orders[0].price:
-				o.order_security.inner_ask = ask_orders[0].price
-			elif o.bidask == 'ASK':
-				o.order_security.inner_ask = o.price
-			if o.bidask == 'BID' and o.price > bid_orders[0].price:
-				o.order_security.inner_bid = bid_orders[0].price
-			elif o.bidask == 'BID':
-				o.order_security.inner_bid = bid_orders[0].price
+			o.order_security.inner_ask = ask_orders[0].price
+			o.order_security.inner_bid = bid_orders[0].price
 			o.order_security.save()
 			context = {
 				'order':o,
