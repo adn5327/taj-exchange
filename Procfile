6dc153df2399063cb77c4d06d59443e00c785cd7
Abhishek Deep Nigam <adn5327@gmail.com>
2016-04-11 11:36:53 -0500

    adding settings for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn taj.wsgi --log-file -
--- tajsite/tajsite/settings.py
+++ tajsite/tajsite/settings.py
@@ -10,11 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os
+import os,dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
+STATIC_FILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -75,17 +82,12 @@ WSGI_APPLICATION = 'tajsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tajexcha_exchange',
-        'USER': 'tajexcha_admin',
-        'PASSWORD': os.environ['PW411'],
-        'HOST': 'web.engr.illinois.edu',
-        'PORT': '3306',
     }
 }
-
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
