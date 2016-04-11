49b1f7bdbe8c3e6f2d307bf9558ac0f711b3ae67
Tyler <tylersplitt@gmail.com>
2016-04-11 15:13:38 -0500

    Dropped tables and remigrated

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Security, Order, Account
+from .models import Security, Order, Account, Possessions, Trade
 
 from django.contrib.auth.models import User, Permission, Group
 
@@ -8,4 +8,7 @@ from django.contrib.auth.models import User, Permission, Group
 admin.site.register(Security)
 admin.site.register(Order)
 admin.site.register(Account)
+admin.site.register(Possessions)
+admin.site.register(Trade)
+
 
