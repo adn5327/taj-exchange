f66b12f9a34a2314aa98c0faecb61aa48b0b5573
Abhishek Deep Nigam <adn5327@gmail.com>
2016-04-17 14:43:31 -0500

    adding template for sector recommendation

---
+++ exchange/sector_rec.py
@@ -0,0 +1,24 @@
+risk_map = {
+            'Technology': .85,
+            'Financials': .77,
+            'Energy':.73,
+            'Healthcare':.57,
+            'Telecom':.48,
+            'Cyclical Goods':.40,
+            'Industrials':.30,
+            'NonCyclical Goods': .20,
+            'Utilities':.18,
+            'Basic Materials':.15,
+            }
+
+def calculate_current_risk(account):
+    pass
+
+def aggressive():
+    pass
+
+def moderate():
+    pass
+
+def safe():
+    pass
