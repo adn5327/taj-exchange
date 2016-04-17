d9b7f36418395ce19f13d4246ec7dcd663e8faae
Jakub Klapacz <kubaklapacz@gmail.com>
2016-04-17 15:37:17 -0500

    Added global variables for sector_rec

--- exchange/sector_rec.py
+++ exchange/sector_rec.py
@@ -1,5 +1,9 @@
 from .models import *
 
+
+percent_modification = .25
+risk_change = 1
+
 risk_map = {
             'Technology': .85,
             'Financials': .77,
@@ -13,6 +17,7 @@ risk_map = {
             'Basic Materials':.15,
             }
 
+
 def calculate_current_risk(account):
     account_possessions = Possessions.objects.filter(account_id=account)
     risk = 0
@@ -23,9 +28,13 @@ def calculate_current_risk(account):
         risk += pos_risk
     # risk = risk * 10
     risk = (float(risk) / float(total_possessions))*10
-    return risk
+    return risk, total_possessions
+
+def computeStrategy(depth, risk, total_possessions):
+    raw_risk = risk / 10 
 
 def aggressive():
+    return computeStrategy()
     pass
 
 def moderate():
