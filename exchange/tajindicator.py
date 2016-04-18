b6f6068bd7eb46f3f54e1bcc0a004a497a24f18a
Abhishek Deep Nigam <adn5327@gmail.com>
2016-04-17 20:33:57 -0500

    update tajindicator

--- exchange/tajindicator.py
+++ exchange/tajindicator.py
@@ -4,9 +4,15 @@ from .models import *
 def calc_taj(sec_symbol):
 	cur_security = Security.objects.get(symbol=sec_symbol)
 
+def voting_avg(cur_security):
+	weight_avg = weighted_average(cur_security)
+	return 10*((weight_avg - cur_security.fmv)/ cur_security.fmv)
+
 def weighted_average(cur_security):
 	bids = Order.objects.filter(order_security=cur_security,bidask='BID').order_by('-price')[:5]
 	asks = Order.objects.filter(order_security=cur_security,bidask='ASK').order_by('price')[:5]
+	if len(bids) <5 or len(asks) <5:
+		return 0
 	bid_sum = bids.aggregate(total_sum=Sum('amount'))
 	ask_sum = asks.aggregate(total_sum=Sum('amount'))
 	total_amount = bid_sum['total_sum'] + ask_sum['total_sum']
@@ -19,12 +25,14 @@ def weighted_average(cur_security):
 
 def top_orders(cur_security):
 	top = Order.objects.filter(order_security=cur_security).order_by('-amount')[:3]
+	if len(top) <3:
+		return 0
 	top_sum = top.aggregate(total_sum=Sum('amount'))
 	total_pd = 0
 	bids = 0
 	asks = 0
 	for each_top in top:
-		weight = each_top.amount/top_sum
+		weight = each_top.amount/top_sum['total_sum']
 		percent_diff = (each_top.price-cur_security.fmv)/cur_security.fmv
 		total_pd = total_pd + weight*percent_diff
 		if each_top.bidask == "BID":
